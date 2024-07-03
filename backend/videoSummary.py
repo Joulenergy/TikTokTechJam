e7309da43f7bbbf2bce8a054859c5854b29b649f
Ryan <ryanyeojl@gmail.com>
2024-07-03 20:24:16 +0800

    Integrate Video Summariser as an Endpoint
    End tests for comment scrape and video summariser
    Added downloadModel script to save model locally for faster inference
    Quantized Llava-next model
    Update requirement.txt

---
+++ backend/videoSummary.py
@@ -0,0 +1,72 @@
+import os
+import pyktok as pyk
+import av
+import torch
+import numpy as np
+from .model import Model
+
+class VideoSummary:
+    def __init__(self):
+        self.model = Model.load_model()
+        self.processor = Model.load_processor()
+
+    def get_video(self, videoLink):
+        pyk.save_tiktok(videoLink, True, 'data.csv')
+        for filename in os.listdir(os.getcwd()):
+            if filename.endswith(".mp4"):
+                return filename
+        return None
+
+    def read_video_pyav(self, container, indices):
+        frames = []
+        container.seek(0)
+        start_index = indices[0]
+        end_index = indices[-1]
+        for i, frame in enumerate(container.decode(video=0)):
+            if i > end_index:
+                break
+            if i >= start_index and i in indices:
+                frames.append(frame)
+        return np.stack([x.to_ndarray(format="rgb24") for x in frames])
+
+    def explain_video(self, videoPath):
+        
+        conversation = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Summarize the contents of this video very briefly while including any text shown in the video in less than a 100 tokens"},
+                    {"type": "video"},
+                ],
+            }
+        ]
+        prompt = self.processor.apply_chat_template(conversation, add_generation_prompt=True)
+
+        container = av.open(videoPath)
+        framesSampled = 8
+        total_frames = container.streams.video[0].frames
+        indices = np.arange(0, total_frames, total_frames / framesSampled).astype(int)
+        clip = self.read_video_pyav(container, indices)
+
+
+        inputs_video = self.processor(text=prompt, videos=clip, padding=True, return_tensors="pt").to(self.model.device)
+
+        output = self.model.generate(**inputs_video, max_new_tokens=150, do_sample=False)
+
+        unprocessed_text = self.processor.decode(output[0], skip_special_tokens=True)
+
+        # Close the container
+        container.close()
+
+        # Return text after "ASSISTANT:"
+        return unprocessed_text.split("ASSISTANT:")[1].strip()
+
+    def clean_up(self, filename):
+        os.remove(filename)
+        os.remove('data.csv')
+
+    def get_video_summary(self, link):
+        filename = self.get_video(link)
+        summary = self.explain_video(filename)
+        self.clean_up(filename)
+        return {"summary": summary}
--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -1,14 +1,14 @@
 import pytest
 
-# Test the /videos/comments/ endpoint
+# Unit Testing for endpoints
 @pytest.mark.parametrize('urls',  
-        [
-            "https://www.tiktok.com/@youcureofficial/video/7363477383309053226",
-            "https://www.tiktok.com/@blogilates/video/7118207082913942826",
-            "https://www.tiktok.com/@naraazizasmith/video/7382679252577029422",
-            "https://www.tiktok.com/@blogilates/video/7370460897103449386",
-            "https://www.tiktok.com/@youcureofficial/video/7384306298176785707",
-        ])
+    [
+        "https://www.tiktok.com/@youcureofficial/video/7363477383309053226",
+        "https://www.tiktok.com/@blogilates/video/7118207082913942826",
+        "https://www.tiktok.com/@naraazizasmith/video/7382679252577029422",
+        "https://www.tiktok.com/@blogilates/video/7370460897103449386",
+        "https://www.tiktok.com/@youcureofficial/video/7384306298176785707",
+    ])
 def test_scrape_comments(client, urls, capsys):
     endpoint = "/videos/comments/"
 
@@ -23,6 +23,30 @@ def test_scrape_comments(client, urls, capsys):
         assert urls in response.json()['result']
         # Make sure response JSON is in the expected format
         assert 'comments' in response.json()['result'][urls]
+        print(response.json()['result'][urls]['comments'])
         assert 'comment_count' in response.json()['result'][urls]
+
+@pytest.mark.parametrize('urls',  
+    [
+        "https://www.tiktok.com/@youcureofficial/video/7363477383309053226",
+        "https://www.tiktok.com/@blogilates/video/7118207082913942826",
+        "https://www.tiktok.com/@naraazizasmith/video/7382679252577029422",
+        "https://www.tiktok.com/@blogilates/video/7370460897103449386",
+        "https://www.tiktok.com/@youcureofficial/video/7384306298176785707",
+    ])
+def test_summarise_videos(client, urls, capsys):
+    endpoint = "/videos/summarise/"
+
+    with capsys.disabled():
+        payload = {"URLS": [urls]}
+
+        response = client.post(endpoint, json=payload)
+
+        assert response.status_code == 200
+        # Make sure that the payload is part of the response
+        assert urls in response.json()['result']
+        print(response.json()['result'][urls]['summary'])
+        # Make sure response JSON is in the expected format
+        assert 'summary' in response.json()['result'][urls]
 
 
