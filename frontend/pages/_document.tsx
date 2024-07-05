ae112d8f3a71c3e164519b3bd5ee375a8d1f1e18
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2024-07-05 17:17:17 +0800

    refactor(cleaned comments)

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,20 +1,3 @@
-// import { Html, Head, Main, NextScript } from 'next/document';
-// import { ColorSchemeScript } from '@mantine/core';
-
-// export default function Document() {
-//   return (
-//     <Html lang="en">
-//       <Head>
-//         <ColorSchemeScript />
-//       </Head>
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   );
-// }
-
 import { Head, Html, Main, NextScript } from 'next/document';
 import { ColorSchemeScript } from '@mantine/core';
 
